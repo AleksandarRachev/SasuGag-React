7a64825e9a16970093279987de83663bb002df9e
Aleksandar Rachev <rachev.96@abv.bg>
2019-11-17 23:39:45 +0200

    remade login buttons to a dropdown

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddPostPage from './pages/AddPostPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PostPage from './pages/PostPage';
+import menuicon from './icons/menuicon1.png';
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,37 +20,44 @@ class App extends React.Component {
   }
 
   renderLogin = () => {
-      if (localStorage.getItem("token") == null) {
-        return (
-          <a className="regular-navbar-link" href="/login">Login</a>
-        );
-      } else {
-        return (
-          <a className="regular-navbar-link" onClick={this.logout.bind(this)} href=".">Logout</a>
-        );
-      }
+    if (localStorage.getItem("token") == null) {
+      return (
+        <a href="/login">Login</a>
+      );
+    } else {
+      return (
+        <a onClick={this.logout.bind(this)} href=".">Logout</a>
+      );
     }
+  }
 
-  render(){
-  return (
-    <div className="App-header">
-      <div className="navbar">
-        <a className="home-link" href="/">Home</a>
-        {this.renderLogin()}
-        <a className="regular-navbar-link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Never</a>
+
+  render() {
+    return (
+      <div className="App-header">
+        <div className="navbar">
+          <a className="home-link" href="/">Home</a>
+          <div class="dropdown">
+            {/* <button class="dropbtn">Dropdown</button> */}
+            <img class="dropbtn" src={menuicon}/>
+            <div class="dropdown-content">
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Never</a>
+              {this.renderLogin()}
+            </div>
+          </div>
+        </div>
+        <Router>
+          <Switch>
+            <Route path="/home"><HomePage /></Route>
+            <Route path="/post-add"><AddPostPage /></Route>
+            <Route path="/login"><LoginPage /></Route>
+            <Route path="/register"><RegisterPage /></Route>
+            <Route path="/post**"><PostPage /></Route>
+            <Redirect from="/" to="/home"></Redirect>
+          </Switch>
+        </Router>
       </div>
-      <Router>
-        <Switch>
-          <Route path="/home"><HomePage /></Route>
-          <Route path="/post-add"><AddPostPage /></Route>
-          <Route path="/login"><LoginPage /></Route>
-          <Route path="/register"><RegisterPage /></Route>
-          <Route path="/post**"><PostPage/></Route>
-          <Redirect from="/" to="/home"></Redirect>
-        </Switch>
-      </Router>
-    </div>
-  );
+    );
   }
 }
 

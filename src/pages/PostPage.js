bbfd85e0356c4112642dedb34acd59cb04d311fd
Aleksandar Rachev <aleksandar_rachev@epam.com>
2019-11-19 13:14:08 +0200

    added buttons and points in post overview

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import GlobalVariables from '../globalVariables';
-import '../css/ProductPage.css'
+import '../css/ProductPage.css';
+import upvote from '../icons/upvote.png';
+import downvote from '../icons/downvote.png';
 
 class PostPage extends React.Component {
 
@@ -18,18 +20,33 @@ class PostPage extends React.Component {
         axios.get(GlobalVariables.backendUrl + "/posts/" + postId, {}).then(data => this.setState({ ...this.state, post: data.data }));
     }
 
+    votePost = (postId, action) => {
+        axios.put(GlobalVariables.backendUrl+"/posts/vote", {
+            uid: postId,
+            vote: action
+        }).then(data => this.setState({...this.state, post: data.data}));
+    }
+
     render() {
 
         if (this.state.post) {
             return (
                 <div className="single-post">
                     <h1>{this.state.post != null ? this.state.post.title : ""}</h1>
-                            <p>{this.state.post.userUsername != null ? "by " + this.state.post.userUsername : ""}</p>
+                    <p>{this.state.post.userUsername != null ? "by " + this.state.post.userUsername : ""}</p>
                     <img className="image" alt={this.state.post.title} src={GlobalVariables.backendUrl + '/posts/image/' + this.state.post.uid} width="350" />
+                    <div className="info">
+                        <p className="points">{this.state.post.points} points . </p>
+                        <p className="points">{0} comments</p>
+                    </div>
+                    <div className="post-buttons">
+                        <img className="vote-button" src={upvote} onClick={this.votePost.bind(this, this.state.post.uid, "up")} />
+                        <img className="vote-button" src={downvote} onClick={this.votePost.bind(this, this.state.post.uid, "down")} />
+                    </div>
                 </div>
             );
         }
-        else{
+        else {
             return null;
         }
 
